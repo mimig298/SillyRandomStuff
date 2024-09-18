d02ffe7ff4a0260e196464296c41edffa6858a42
mimig298 <miguelldvmendes@gmail.com>
2024-09-18 20:01:52 +0100

    Add project files.

---
+++ Content/Items/Weapons/BasicSword.cs
@@ -0,0 +1,25 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SillyRandomStuff.Content.Items.Weapons
+{
+    public class BasicSword : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 50;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 20;
+            Item.useAnimation = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 6;
+            Item.value = Item.buyPrice(silver: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+    }
+}
