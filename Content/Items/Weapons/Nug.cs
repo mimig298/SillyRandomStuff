f52f4f50e5dc69313762ffdd6dcb3bcf09d18ccf
mimig298 <miguelldvmendes@gmail.com>
2024-09-22 17:01:27 +0100

    Nug

---
+++ Content/Items/Weapons/Nug.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SillyRandomStuff.Content.Items.Weapons
+{
+    public class Nug : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 50;
+            Item.height = 26;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = 37;
+            Item.useAnimation = 37;
+
+            Item.DamageType = DamageClass.Ranged;
+            Item.damage = 30;
+            Item.knockBack = 0f;
+            Item.noMelee = true;
+
+            Item.rare = ItemRarityID.Green;
+            Item.UseSound = SoundID.Item11;
+
+            Item.shoot = ProjectileID.PurificationPowder;
+            Item.shootSpeed = 5f;
+            Item.useAmmo = AmmoID.Bullet;
+        }
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            velocity = new Vector2(velocity.Y, velocity.X) * -(knockback + 1);
+            (damage, type) = (type, damage);
+        }
+    }
+}
